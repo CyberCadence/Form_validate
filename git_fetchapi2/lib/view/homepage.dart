7b4e4f22dafd6e913ed0ce05148abeae6df668f1
CyberCadence <diljithv7@gmail.com>
2022-10-15 17:48:41 +0530

    form created with bloc

--- git_fetchapi2/lib/view/homepage.dart
+++ git_fetchapi2/lib/view/homepage.dart
@@ -1,5 +1,6 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:git_fetchapi2/view/bloc/formm_bloc.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -26,51 +27,73 @@ class _MyAppState extends State<MyApp> {
           ),
           body: SafeArea(
               child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-            children: [
-             
-TextField(controller: titletextEditingController,
-  decoration: InputDecoration(hintText: 'Title',
-    enabledBorder: OutlineInputBorder(
-      borderSide:
-          const BorderSide(width: 1, color: Colors.greenAccent),  
-      borderRadius: BorderRadius.circular(30.0),
-    ),
-  ),
-),const SizedBox(height: 10,),
-TextField(
-  decoration: InputDecoration(hintText: 'Details',
-    enabledBorder: OutlineInputBorder(
-      borderSide:
-          const BorderSide(width: 1, color: Colors.greenAccent),  
-      borderRadius: BorderRadius.circular(30.0),
-    ),
-  ),
-controller: descriptiontextEditingController,),const SizedBox(height: 10,),
-TextField(controller: locationtextEditingController,
-  decoration: InputDecoration(hintText: 'Location',
-    enabledBorder: OutlineInputBorder(
-      borderSide:
-          const BorderSide(width: 1, color: Colors.greenAccent), 
-      borderRadius: BorderRadius.circular(30.0),
-    ),
-  ),
-),const SizedBox(height: 10,),
-TextField(controller: datetextEditingController,
-  decoration: InputDecoration(hintText: 'Date',
-    enabledBorder: OutlineInputBorder(
-      borderSide:
-          const BorderSide(width: 1, color: Colors.greenAccent),  
-      borderRadius: BorderRadius.circular(30.0),
-    ),
-  ),
-),const SizedBox(height: 10,),
-         ElevatedButton(
-          child: const Text('SUBMIT'),
-          onPressed: ( ){})   ],
-          ),
-              ))),
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                TextField(
+                  controller: titletextEditingController,
+                  decoration: InputDecoration(
+                    hintText: 'Title',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(width: 1, color: Colors.greenAccent),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Details',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(width: 1, color: Colors.greenAccent),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                  ),
+                  controller: descriptiontextEditingController,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  controller: locationtextEditingController,
+                  decoration: InputDecoration(
+                    hintText: 'Location',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(width: 1, color: Colors.greenAccent),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  controller: datetextEditingController,
+                  decoration: InputDecoration(
+                    hintText: 'Date',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(width: 1, color: Colors.greenAccent),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+            
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(child: const Text('SUBMIT'), onPressed: () {})
+              ],
+            ),
+          ))),
     );
   }
 }
